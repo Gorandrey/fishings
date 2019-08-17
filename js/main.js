7872a1ddeec4c6510a0e2f594332e6e8fbefdf73
Andrey <gorand.one@gmail.com>
2019-08-17 12:54:22 +0300

    fishing

---
+++ js/main.js
@@ -0,0 +1,66 @@
+
+
+  $(function() {
+
+      // reload tab-index
+      
+        const url = document.location.toString();
+        if (url.match('#')) {
+            $('.nav-tabs a[href="#' + url.split('#')[1] + '"]').tab('show');
+            
+        }  
+      
+      //open tab from menu
+      $('.menu-link').on('click', function(){
+        
+        const hrf = $(this).attr('href');
+      
+        if (hrf.match('#')) {
+            
+            $('.nav-tabs a[href="#' + hrf.split('#')[1] + '"]').tab('show');
+            $(this).closest('.left-menu').removeClass('nav-show');
+            
+        }
+         
+      })
+      $('.nav-tabs a').on('shown.bs.tab', function (e) {
+        window.location = url.split('#')[0]+e.target.hash;
+    }); 
+      
+  	//show menu
+
+    $('.menu-icon').on('click', function() {
+    	
+      $(this).closest('.left-menu')
+        .toggleClass('nav-show', 1000);
+    });
+
+
+
+	// video
+
+	$('.play').on('click', function(e){
+		e.preventDefault();
+		const vid = $(this).prev()[0];
+
+		if (vid.paused){
+           vid.play();
+           vid.controls = "controls";
+           $(this).hide();
+		}
+        else{
+            vid.pause()
+        }
+		
+	});
+	$('.video').on('click', function(e){
+			e.preventDefault();
+           $(this)[0].pause();
+           $(this)[0].controls = null;
+           $(this).next().show();
+	
+	});
+
+    
+    
+  });
